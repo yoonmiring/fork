d20788e3ed115331af87b8ecf7848d0d938ca001
ymjeong <ymjeong@unours.co.kr>
2022-08-11 13:51:15 +0900

    scanner

--- test2.java
+++ test2.java
@@ -1,7 +1,14 @@
+import static java.lang.System.out;
+
+import java.util.Scanner;
+
 public class test2 {
     public static void main(String[] args){
     System.out.println("good");
     System.out.println("excellent");
-
+    Scanner sc = new Scanner(System.in);
+    int a= sc.nextInt();
+    int b= sc.nextInt();
+    System.out.println(a+b);
     }
 }
