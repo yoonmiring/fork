132fdf6366cdf5afee14235a33f605e8bcc8a01d
ymjeong <ymjeong@unours.co.kr>
2022-08-11 15:49:04 +0900

    stack algorithm

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i < T;i++){
+            sb.append(solve(br.readLine())).append('\n');
+        }  
+        System.out.println(sb);
+    }
+    public static String solve(String VPS) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int j =0;j<VPS.length();j++){
+            char c = VPS.charAt(j);
+
+            if(c =='('){
+                stack.push(c);
+            }else if(stack.empty()){
+                return "NO";
+            }else{
+                stack.pop();
+            }
+        }
+        if(stack.empty()){
+            return "YES";
+        }else{
+            return "NO";
+        }
+        
+    }
+
+    
+}
