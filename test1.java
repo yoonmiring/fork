2f51ebf44a554ea415324707d4514218f36f0518
ymjeong <ymjeong@unours.co.kr>
2022-08-11 11:06:06 +0900

    finally

--- test1.java
+++ test1.java
@@ -1,6 +1,9 @@
 public class test1 {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("good world");
+        System.out.println("check");
+        System.out.println("123");
     }
     
 }
---
+++ test2.java
@@ -0,0 +1,7 @@
+public class test2 {
+    public static void main(String[] args){
+    System.out.println("good");
+    System.out.println("excellent");
+
+    }
+}
