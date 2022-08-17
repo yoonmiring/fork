562bd767311dc486e2adda968e2ecc3b9e14210a
ymjeong <ymjeong@unours.co.kr>
2022-08-17 14:39:21 +0900

    queue 카드1

---
+++ aa2161.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class aa2161 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        LinkedList<Integer> list = new LinkedList<>();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            list.offer(i);
+        }
+        while (list.size() > 1) {
+            System.out.print(list.poll() + " ");
+            list.offer(list.poll());
+        }
+        System.out.print(list.poll());
+    }
+}
